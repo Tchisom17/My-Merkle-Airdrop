7e98f1d62da8ba3187b5fba9027e4bb8b6a020af
Tchisom <Kamsiyochukwu1@gmail.com>
2024-10-04 01:35:23 +0100

    feat: Implement MerkleTreeAirdrop with claim and withdraw functions

--- contracts/MerkleTreeAirdrop.sol
+++ contracts/MerkleTreeAirdrop.sol
@@ -2,15 +2,15 @@
 
 pragma solidity ^0.8.0;
 
-import "./MyNFT.sol";
 import "./MyToken.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract MerkleTreeAirdrop {
     using SafeERC20 for MyToken;
 
-    MyNFT public myNFT;
+    IERC721 public baycNFT;
     MyToken public immutable myToken;
 
     bytes32 public immutable merkleRoot;
@@ -27,16 +27,16 @@ contract MerkleTreeAirdrop {
         require(owner == msg.sender, "You are not the owner");
         _;
     }
-    constructor (MyToken _token, MyNFT _myNft, bytes32 _merkleRoot, uint256 _totalAirdropSuply) {
+    constructor (MyToken _token, address _baycNft, bytes32 _merkleRoot, uint256 _totalAirdropSuply) {
         myToken = _token;
-        myNFT = _myNft;
+        baycNFT = IERC721(_baycNft);
         merkleRoot = _merkleRoot;
         owner = msg.sender;
         totalAirdropSupply = _totalAirdropSuply;
     }
 
     function claim(uint _amount, bytes32[] calldata merkleProof) external {
-        require(canClaim(msg.sender, _amount, merkleProof), "Sorry, Your address is not eligible to claim token!");
+        require(canClaim(msg.sender, _amount, merkleProof), "You are not eligible!");
 
         claimed[msg.sender] = true;
         totalClaimed += _amount;
@@ -46,28 +46,7 @@ contract MerkleTreeAirdrop {
         emit Claimed(msg.sender, _amount);
     }
 
-    function canClaim(address claimer, uint _amount, bytes32[] calldata merkleProof) public view returns (bool) {
-        require(!claimed[claimer], "Token already claimed!");
-
-        bytes32 leaf = keccak256(abi.encodePacked(msg.sender, _amount));
-
-        require(MerkleProof.verify(merkleProof, merkleRoot, leaf), "Invalid proof!");
-        require(totalClaimed + _amount <= totalAirdropSupply, "Exceeds total airdrop supply!");
-
-        return true;
-    }
-
-    // function setMerkleRoot(bytes32 _merkleRoot) external onlyOwner {
-    //     merkleRoot = _merkleRoot;
-    //     emit MerkleRootUpdated(merkleRoot);
-    // }
-
     function withdrawToken(address to) external onlyOwner {
-        // uint256 balance = token.balanceOf(address(this));
-        // require(balance > 0, "No tokens to withdraw.");
-        // require(isAirdropComplete(), "Airdrop is not complete.");
-
-        // require(token.transfer(to, balance), "Token transfer failed.");
 
         require(checkAirdropStatus(), "Airdrop is not complete!");
         
@@ -79,5 +58,22 @@ contract MerkleTreeAirdrop {
 
     function checkAirdropStatus() internal  view returns(bool) {
         return totalClaimed >= totalAirdropSupply;
+    }
+
+    // function setNFTOwnerBalance(address _owner) external {
+    //     baycNFT.balanceOf(_owner);
+    //     emit MerkleRootUpdated(merkleRoot);
+    // }
+
+    function canClaim(address claimer, uint _amount, bytes32[] calldata merkleProof) public view returns (bool) {
+        require(!claimed[claimer], "Token already claimed!");
+        require(baycNFT.balanceOf(claimer) > 0, "Must own BAYC NFT");
+
+        bytes32 leaf = keccak256(abi.encodePacked(msg.sender, _amount));
+
+        require(MerkleProof.verify(merkleProof, merkleRoot, leaf), "Invalid proof!");
+        require(totalClaimed + _amount <= totalAirdropSupply, "Exceeds total airdrop supply!");
+
+        return true;
     }
 }
