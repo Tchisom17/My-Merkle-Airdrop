3166d449a6ed5577ade16192aa0edef75d511eeb
Tchisom <Kamsiyochukwu1@gmail.com>
2024-10-04 01:45:45 +0100

    feat: Add MyToken deployment module using hardhat ignition

---
+++ ignition/modules/MyToken.ts
@@ -0,0 +1,9 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const MyTokenModule = buildModule("MyTokenModule", (m) => {
+  const erc20 = m.contract("MyToken");
+
+  return { erc20 };
+});
+
+export default MyTokenModule;
